8dcd483e36d2325d7886e23c33fed020f03f26ae
Yadong Zhu <yadong.zhu@gmail.com>
2018-07-10 13:06:21 +0800

    save json to excel, csv file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # business-reporting
 generate real time reports from SAP
+CLI usage:
+node index.js dev invent-rep 201806
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let reportType,reportYmdate,outputFolder;
 if (process.argv.length <= 4) {
     console.log("Usage: node index.js [dev/qas/prod] [invent-rep] [ymdate (yyyymm)]");
+    console.log("Usage example: node index.js dev invent-rep 201806");
 	process.exit(-1);
 } else {
     let env = process.argv[2];
@@ -13,17 +14,31 @@ if (process.argv.length <= 4) {
 const sapSvc =require('./backend/services/sapService');
 const jsonfile =require('jsonfile');
 const json2xls = require('json2xls');
+const jsonexport = require("jsonexport/dist");
 (async function () {
     try {
-        var args={},result,file,xlsFile;
+        var args={},result,file,xlsFile,csvfile;
         if (reportType==='invent-rep'){
             args.ymdate=reportYmdate;
             result = await sapSvc.getInventoryReport(args);
+            //save json file
             file=outputFolder+"/"+reportType+"-"+reportYmdate+".json";
             jsonfile.writeFileSync(file,result["INVENT_REPORT"]);
+            //save excel file
             xlsFile=outputFolder+"/"+reportType+"-"+reportYmdate+".xlsx";
             let xls = json2xls(result["INVENT_REPORT"]);
             require('fs').writeFileSync(xlsFile,xls, 'binary');
+            //sve csv file
+            csvfile=outputFolder+"/"+reportType+"-"+reportYmdate+".csv";
+            jsonexport(result["INVENT_REPORT"],function(err,csv){
+                if (err){
+                    return console.error(err);
+                } else {
+                    require('fs').writeFileSync(csvfile,csv)
+                }
+            })
+            
+
         }
     } catch (error) {
         console.error(error);
